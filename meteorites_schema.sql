fd40dceb4edeea9fca47c78bffe47a50f61fc62c
ldevlysh314 <145075505+ldevlysh314@users.noreply.github.com>
2024-01-16 19:54:46 -0600

    Add files via upload

---
+++ meteorites_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "meteorites" (
+	id serial PRIMARY KEY,
+    "name" varchar(50)   NOT NULL,
+    "class" varchar(40)   NOT NULL,
+    "mass" numeric  NOT NULL,
+    "fall" varchar(10)   NOT NULL,
+    "year" integer  NOT NULL,
+    "latitude" double precision   NOT NULL,
+    "longitude" double precision   NOT NULL
+);
+drop table "meteorites";
+
+select * from meteorites
